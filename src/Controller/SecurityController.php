0caccb4c5fb079c1b29b6db876334f2074b09193
Christophe LESIEUR <chrlesieur@free.fr>
2019-01-19 18:21:10 +0100

    authentification suite

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -9,6 +9,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoder;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -25,16 +27,23 @@ class SecurityController extends AbstractController
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
+
     /**
      * @Route("/register", name="app_register")
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     * @param UserPasswordEncoderInterface $userPasswordEncoder
+     * @return Response
      */
-    public function register(Request $request, EntityManagerInterface $em): Response
+    public function register(Request $request, EntityManagerInterface $em, UserPasswordEncoderInterface
+    $userPasswordEncoder): Response
     {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword($userPasswordEncoder->encodePassword($user, $user->getPassword()));
             $em->persist($user);
             $em->flush();
         }
--- src/Service/MovieApiRequest.php
+++ src/Service/MovieApiRequest.php
@@ -8,8 +8,16 @@
 
 namespace App\Service;
 
+use GuzzleHttp\Client;
 
 class MovieApiRequest
 {
+    public function getDetailsApi($title)
+    {
+        $client = new Client();
+        $res = $client->request('GET', 'http://www.omdbapi.com/?t='.$title.'apikey=54fcec65');
 
+        return $data = json_decode($res->getBody(), true);
+
+    }
 }
