a9ed03cca531a40a98ded6ef161eb819d64048ae
Christophe LESIEUR <chrlesieur@free.fr>
2019-01-21 14:20:01 +0100

    crud user fonctionnel

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-
+/**
+ * @IsGranted("ROLE_USER")
+ */
 class HomeController extends AbstractController
 {
     /**
